1635c4b9849c8a0cef241245caae9792af85ae80
Frederico Leles Santos <fred.leles@hotmail.com>
2022-07-07 13:11:31 -0300

    feat: Cria os componentes e configura os bidinds da lista de tabelas

--- MFEstrategyMaker/ViewModels/RegisterViewModel/CTabTabelasViewModel.cs
+++ MFEstrategyMaker/ViewModels/RegisterViewModel/CTabTabelasViewModel.cs
@@ -2,6 +2,7 @@
 using MFEstrategyMaker.Stores;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,19 @@ namespace MFEstrategyMaker.ViewModels.RegisterViewModel
     public class CTabTabelasViewModel : CTabItemViewModel
     {
         public ICommand NewDBTable { get; }
+        private readonly ObservableCollection<DbTableNamesListCompVM> _dbTableNamesListCompVM;
+        public IEnumerable<DbTableNamesListCompVM> DbTableNamesListCompVM => _dbTableNamesListCompVM;
         public CTabTabelasViewModel(NavigationStore navigationStore)
         {
             TabName = "Tabelas";
 
             NewDBTable = new OpenModalCommand<AddNewDBTableVM>(navigationStore,
                 () => new AddNewDBTableVM(navigationStore));
+
+            _dbTableNamesListCompVM = new ObservableCollection<DbTableNamesListCompVM>();
+
+            _dbTableNamesListCompVM.Add(new DbTableNamesListCompVM("Constantes"));
+            _dbTableNamesListCompVM.Add(new DbTableNamesListCompVM("Reuniões_COPOM"));
         }
     }
 }
---
+++ MFEstrategyMaker/ViewModels/RegisterViewModel/DbTableNamesListCompVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MFEstrategyMaker.ViewModels.RegisterViewModel
+{
+    public class DbTableNamesListCompVM : ViewModelBase
+    {
+        public string TableName { get; }
+        public ICommand EditCommand { get; }
+        public ICommand DropCommand { get; }
+
+        public DbTableNamesListCompVM(string tableName)
+        {
+            TableName = tableName;
+        }
+    }
+}
